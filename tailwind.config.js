d9f171ce30f0dfc16c488c1c66eeb41157cc3a27
Sandeep Sharma <1949sandeepsharma52@gmail.com>
2023-05-19 06:57:20 +0545

    [init] initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				primaryClr:
+					"linear-gradient(92.73deg,#ff5872 5.58%,#e20bc3 51.02%,#883fff 94.61%)",
+			},
+		},
+	},
+	plugins: [],
+};
