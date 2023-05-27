b0ae9fceca6875ef018fa2d615ba3030c57d3b73
Sandeep Sharma <1949sandeepsharma52@gmail.com>
2023-05-27 17:47:15 +0545

    new text field group input component added

--- src/components/Shared/Inputs/TextFields.tsx
+++ src/components/Shared/Inputs/TextFields.tsx
@@ -26,3 +26,46 @@ export const TextField = (props: any) => {
 		</div>
 	);
 };
+
+export const TextFieldGroup = (props: any) => {
+	const {
+		text,
+		name,
+		type,
+		placeholder,
+		value,
+		error,
+		required,
+		className,
+		groupText,
+	} = props;
+	return (
+		<div className="flex flex-col gap-1.5 w-full">
+			<label className="text-md font-md" htmlFor={name}>
+				{text} {required && <span className="text-red-500">*</span>}
+			</label>
+			<div className="relative flex flex-wrap items-stretch">
+				<input
+					type={type}
+					name={name}
+					id={name}
+					placeholder={placeholder}
+					value={value}
+					className={`p-1.5 text-lg border rounded-md rounded-r-none text-gray-500 tracking-wider transition-colors duration-100 w-100 focus:outline-2 focus:border-0 ${
+						error
+							? `border-red-500 focus:outline-red-300 hover:border-red-300`
+							: `border-gray-300 focus:border-purple-300`
+					} ${className} flex-grow`}
+				/>
+				<span className="flex items-center rounded-r-md border border-l-0 border-gray-400 tracking-wider p-1.5 text-center text-lg bg-gray-400 text-white">
+					{groupText}
+				</span>
+			</div>
+			{error && (
+				<label htmlFor={name} className="text-red-500">
+					{error}
+				</label>
+			)}
+		</div>
+	);
+};
