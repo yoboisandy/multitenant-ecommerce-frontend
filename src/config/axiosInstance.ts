885cc111fab7a470d9d5018ea17caa598373bf5a
Sandeep Sharma <1949sandeepsharma52@gmail.com>
2023-06-02 22:25:07 +0545

    created axios instance

---
+++ src/config/axiosInstance.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+const axiosInstance = axios.create({
+	timeout: 1000,
+	headers: {
+		Accept: "application/json",
+		"Content-Type": "application/json",
+	},
+});
+
+axiosInstance.interceptors.request.use(
+	function (config) {
+		config.headers.Authorization = `Bearer ${localStorage.getItem(
+			"token"
+		)}`;
+		return config;
+	},
+	function (error) {
+		return Promise.reject(error);
+	}
+);
+
+export default axiosInstance;
