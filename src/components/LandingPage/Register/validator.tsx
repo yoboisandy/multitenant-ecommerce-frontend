3b09746e825b9f263bdcb4e2d51576e372b71056
Sandeep Sharma <1949sandeepsharma52@gmail.com>
2023-06-06 22:09:36 +0545

    updated validation messages for register page

--- src/components/LandingPage/Register/validator.tsx
+++ src/components/LandingPage/Register/validator.tsx
@@ -1,21 +1,21 @@
 import * as yup from "yup";
 export const RegisterFormValidationSchema = yup.object().shape({
-	store_name: yup.string().required(),
+	store_name: yup.string().required("Store name is a required field"),
 	subdomain: yup.string().required(),
-	store_category_id: yup.string().required(),
-	first_name: yup.string().required(),
-	last_name: yup.string().required(),
-	phone: yup.string().required().min(10),
-	email: yup.string().required().email(),
+	store_category_id: yup.string().required("Category is a required field"),
+	first_name: yup.string().required("First name is a required field"),
+	last_name: yup.string().required("Last name is a required field"),
+	phone: yup.string().required("Phone number is a required field").min(10),
+	email: yup.string().required("Email is a required field").email(),
 	password: yup
 		.string()
-		.required()
+		.required("Password is a required field")
 		.matches(
 			/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%#*?&]/,
 			"Must contain 8 characters, one uppercase, one lowercase, one number and one special case character"
 		),
 	confirm_password: yup
 		.string()
-		.required()
+		.required("Confirm password is a required field")
 		.oneOf([yup.ref("password")], "Passwords must match"),
 });
