bee7974c98b8840738f7cc7a68976dc05afdabce
Sandeep Sharma <1949sandeepsharma52@gmail.com>
2023-06-25 15:48:24 +0545

    updated modal as scrollable

--- src/components/Shared/Modals/Modal.tsx
+++ src/components/Shared/Modals/Modal.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 
-const Modal = ({ children }: any) => {
+const Modal = ({ children, ...rest }: any) => {
+	const { show } = rest;
 	return (
 		<div
-			className="min-w-screen h-screen animated fadeIn faster  fixed  left-0 top-0 flex justify-center items-center inset-0 z-50 outline-none focus:outline-none"
-			id="modal-id"
+			className={`min-w-screen h-screen animated fadeIn faster  fixed  left-0 top-0 ${
+				show ? "flex" : "hidden"
+			} justify-center items-center inset-0 z-50 outline-none focus:outline-none`}
 		>
 			<div className="absolute bg-black opacity-80 inset-0 z-0" />
-			<div className="w-full  max-w-lg relative mx-auto my-auto rounded-xl shadow-lg bg-white ">
-				{/*content*/}
-				<div className="p-5 space-y-4">{children}</div>
+			<div className="max-h-screen overflow-y-auto w-full p-4">
+				<div className="max-w-lg relative mx-auto my-auto rounded-xl shadow-lg bg-white ">
+					{/*content*/}
+					<div className="p-5 space-y-4">{children}</div>
+				</div>
 			</div>
 		</div>
 	);
