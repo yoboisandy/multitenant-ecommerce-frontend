33a37e9ac6fe459f111896b440877e646d443ab2
Sandeep Sharma <1949sandeepsharma52@gmail.com>
2023-07-08 19:23:08 +0545

    checkbox component

---
+++ src/components/Shared/Inputs/Checkbox.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Checkbox = (props: any) => {
+	const { text, name } = props;
+	return (
+		<label className="inline-flex items-center">
+			<input
+				type="checkbox"
+				className="
+        form-checkbox
+        rounded
+        text-dashboardClr
+        shadow-sm
+        focus:ring-0
+        focus:outline-none
+        h-5
+        w-5"
+				name={name}
+			/>
+			<span className="ml-2 text-md font-md">{text}</span>
+		</label>
+	);
+};
+
+export default Checkbox;
