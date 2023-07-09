47b48c61fffe2b7d0555f559ba54c147e091b4a3
Sandeep Sharma <1949sandeepsharma52@gmail.com>
2023-07-09 19:59:53 +0545

    memoized jodit editor field configs

--- src/components/Shared/Inputs/Editor.tsx
+++ src/components/Shared/Inputs/Editor.tsx
@@ -1,5 +1,5 @@
 import JoditEditor from "jodit-react";
-import React, { useRef } from "react";
+import React, { useMemo, useRef } from "react";
 import "./editor.css";
 
 const Editor = (props: any) => {
@@ -7,30 +7,34 @@ const Editor = (props: any) => {
 
 	const editor = useRef(null);
 
-	const config = {
-		uploader: {
-			insertImageAsBase64URI: true,
-		},
-		readOnly: false,
-		placeholder: "write something here.....",
-		maxHeight: 400,
-		zIndex: 0,
-		tabIndex: 1,
-		toolbarSticky: false,
-	};
+	const config = useMemo(() => {
+		return {
+			uploader: {
+				insertImageAsBase64URI: true,
+			},
+			readOnly: false,
+			placeholder: "write something here.....",
+			maxHeight: 400,
+			zIndex: 0,
+			tabIndex: 1,
+			toolbarSticky: false,
+		};
+	}, []);
 
 	return (
 		<div className="flex flex-col gap-1.5 flex-1 relative">
 			<label className="text-md font-md" htmlFor={name}>
 				{text} {required && <span className="text-red-500">*</span>}
 			</label>
-			<div className="prose prose-lg all-initial">
-				<JoditEditor
-					ref={editor}
-					config={config}
-					onBlur={(newContent) => setContent(newContent)}
-					value={content}
-				/>
+			<div className={`${error ? "border border-red-500" : ""}`}>
+				<div className={`prose prose-lg all-initial`}>
+					<JoditEditor
+						ref={editor}
+						config={config}
+						onChange={(newContent) => setContent(newContent)}
+						value=""
+					/>
+				</div>
 			</div>
 			{error && (
 				<label htmlFor={name} className="text-red-500 text-sm">
