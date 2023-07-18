004a6ea5d19edc8f56878e7140734775faa9a395
Sandeep Sharma <1949sandeepsharma52@gmail.com>
2023-07-18 17:09:34 +0545

    conditionally render store home page

---
+++ src/components/LandingPage/Store/LandingPage/StoreLandingPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StoreLandingPage = () => {
+	return <div>StoreLandingPage</div>;
+};
+
+export default StoreLandingPage;
--- src/pages/LandingPage/Home.tsx
+++ src/pages/LandingPage/Home.tsx
@@ -1,6 +1,13 @@
 import HomePage from "../../components/LandingPage/HomePage";
+import StoreLandingPage from "../../components/LandingPage/Store/LandingPage/StoreLandingPage";
+import { currentDomain } from "../../config/urlConfig";
 const Home = () => {
-	return <HomePage />;
+	const currentUrl = currentDomain;
+	if (currentUrl.isTenant) {
+		return <StoreLandingPage />;
+	} else {
+		return <HomePage />;
+	}
 };
 
 export default Home;
