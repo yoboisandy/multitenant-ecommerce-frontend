17959b991c7dce7cb40a4f7c0f63be9c5a610810
Sandeep Sharma <1949sandeepsharma52@gmail.com>
2023-07-21 19:59:36 +0545

    image banner component

---
+++ src/components/LandingPage/Store/Shared/ImageBanners/Banner.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Banner = () => {
+	return (
+		<div className="max-w-7xl mx-auto h-[200px]">
+			<img
+				src="https://hamroelectronics.com.np/images/ads/web9.jpg"
+				alt="728x90 Leaderboard"
+				className="w-full h-full object-cover"
+			/>
+		</div>
+	);
+};
+
+export default Banner;
