5c36f8de427bb632d90dfaa2cb5e95d5fed46fe8
Sandeep Sharma <1949sandeepsharma52@gmail.com>
2023-07-21 09:33:00 +0545

    Topbar component

---
+++ src/components/LandingPage/Store/Shared/TopBar/Topbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Topbar = () => {
+	return (
+		<p className="flex h-10 items-center justify-center bg-storeFrontClr px-4 text-sm font-medium text-white sm:px-6 lg:px-8">
+			Get free delivery on orders over $100
+		</p>
+	);
+};
+
+export default Topbar;
