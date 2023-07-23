fb032d4a460c1b7ae18917ad5cb21a1a66549378
Sandeep Sharma <1949sandeepsharma52@gmail.com>
2023-07-23 12:46:52 +0545

    topbar integration dynamic

--- src/components/LandingPage/Store/Shared/TopBar/Topbar.tsx
+++ src/components/LandingPage/Store/Shared/TopBar/Topbar.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { useAppSelector } from "../../../../../app/hooks";
+import { useNavigate } from "react-router-dom";
 
 const Topbar = () => {
+	const storeState: any = useAppSelector((state) => state.StoreSlice);
+	const navigate = useNavigate();
 	return (
-		<p className="flex h-10 items-center justify-center bg-storeFrontClr px-4 text-sm font-medium text-white sm:px-6 lg:px-8">
-			Get free delivery on orders over $100
+		<p
+			className={`${
+				storeState.current_store?.customization?.topbar_text
+					? "flex"
+					: "hidden"
+			} h-10 items-center justify-center bg-storeFrontClr px-4 text-sm font-medium text-white sm:px-6 lg:px-8 cursor-pointer`}
+			onClick={() =>
+				navigate(storeState.current_store?.customization?.topbar_url)
+			}
+		>
+			{storeState.current_store?.customization?.topbar_text}
 		</p>
 	);
 };
